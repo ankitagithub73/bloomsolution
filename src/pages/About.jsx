44f67b455f72cc4b36363bdf863c0d315ddf5ae1
ankitagithub73 <ankita16702sshah@gmail.com>
2026-06-04 19:09:41 +0530

    changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,9 @@
 function About() {
   return (
     <>
-      <h2>This is about page</h2>
+      
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat
+        About page Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat
         veritatis unde porro provident qui nostrum impedit id iusto? Omnis
         deleniti rerum incidunt accusantium voluptates quam odio nihil quos
         temporibus dolore.
